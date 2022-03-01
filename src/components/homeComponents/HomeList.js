d51f197998115396135dda678abc5e0268940a66
JDC1492 <Jonathan_Cohen10@yahoo.com>
2022-03-01 18:53:54 -0500

    hidden divs added to html file

--- src/components/homeComponents/HomeList.js
+++ src/components/homeComponents/HomeList.js
@@ -3,14 +3,18 @@ import React from "react";
 const Homelist = () => {
   return (
     <>
-      <ul>
-        <li className="homeList dev">Developer</li>
-        <div>
-          <p> Stoicism is pretty interesting. </p>
-        </div>
-        <li className="homeList musc">Musician</li>
-        <li className="homeList gamer">Gamer</li>
-      </ul>
+      <p className="homeList dev">
+        Developer
+        <div className="identifier hidden"></div>
+      </p>
+      <p className="homeList musc">
+        Musician
+        <div className="identifier hidden"></div>
+      </p>
+      <p className="homeList gamer">
+        Gamer
+        <div className="identifier hidden"></div>
+      </p>
     </>
   );
 };
